23281c009fa8b5db66d4c5d427a95df450c200af
Dave Mauldin <dmauldin@gmail.com>
2019-04-22 17:34:28 -0700

    fixes

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const fetch = require("node-fetch")
 const fs = require("fs")
+const path = require("path")
+
+const pageSize = 25
+const addonsDir = './addons'
 
 let urls = []
 let results = []
 let done = false
-const pageSize = 25
+let newDateModified = null
+let lastDateModified = null
 
 for (let i = 0; i < 500; i++) {
   urls.push(
@@ -14,12 +19,17 @@ for (let i = 0; i < 500; i++) {
   )
 }
 
-let newDateModified = null
-let lastDateModified = null
+if (!fs.existsSync(addonsDir)) {
+  fs.mkdirSync(addonsDir)
+}
+
 try {
   lastDateModified = new Date(parseInt(fs.readFileSync("lastDateModified", "utf8"), 10)).getTime()
   newDateModified = lastDateModified
-} catch (err) {}
+} catch (err) {
+  // nothing to do here, really, we just aren't going to set the date because we couldn't find the file
+  console.log(`leaves lastDateModified as ${lastDateModified}`)
+}
 
 // results is only useful for being notified when the reduction is complete as
 // it will only contain the final promise that the array was reduced to
@@ -33,14 +43,14 @@ let promises = urls.reduce((accumulatorPromise, nextUrl, index) => {
         .then(json => {
           console.log(`found ${json.length} addons`)
           if (json.length < pageSize) {
+            // received fewer addons than requested page size = next fetch will have 0
             done = true
           }
           for (let i = 0; i < json.length; i++) {
             const addon = json[i]
             const addonDateModified = Date.parse(addon.dateModified)
-            console.log(`addon: ${addon.slug}`)
-            // we don't have a new modified or this modified is newer than the last new modified
-            // we always want to have a new modified date
+            console.log(`addon: ${addon.slug} - dateModified: ${addonDateModified}`)
+            // set newDateModified to newest date found so far
             if (newDateModified === null || addonDateModified > newDateModified) {
               newDateModified = addonDateModified
             }
@@ -54,7 +64,7 @@ let promises = urls.reduce((accumulatorPromise, nextUrl, index) => {
               const filename = `${addon.id}-${addon.slug}.json`
               console.log(`writing json to file ${filename}`)
               fs.writeFileSync(`addons/${filename}`, JSON.stringify(addon))
-              return results.push(json)
+              results.push(json)
             }
           }
           // TODO: just return the count here? so we don't make a massive array in memory? (or even just resolve)
